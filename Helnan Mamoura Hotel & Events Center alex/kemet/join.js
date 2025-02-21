e5cab42129f7ba58e42f7e9fc59f695c5ac872f2
sherouk-mohamed <sheroukmohamed246@gmail.com>
2025-02-21 23:36:42 +0200

    Hotel Images

---
+++ Helnan Mamoura Hotel & Events Center alex/kemet/join.js
@@ -0,0 +1,37 @@
+const filterButtons = document.querySelectorAll(".destination-dd button");
+const filterableCards = document.querySelectorAll(".cards .card");
+const filterCards = (event) => {
+    const activeButton = document.querySelector(".destination-dd .active");
+    if (activeButton) {
+        activeButton.classList.remove("active");
+    }
+
+
+    event.target.classList.add("active");
+
+    const filterName = event.target.dataset.name;
+
+
+    filterableCards.forEach(card => {
+        if (filterName === "sec1" || card.dataset.name === filterName) {
+            card.classList.remove("hide");  
+        } else {
+            card.classList.add("hide");  
+        }
+    });
+};
+
+
+filterButtons.forEach(button => {
+    button.addEventListener("click", filterCards);
+});
+
+
+const menu = document.querySelector('#mobile-menu')
+const menuLinks = document.querySelector('.navbar__menu')
+
+menu.addEventListener('click',function(){
+   menu.classList.toggle('is-active')
+   menuLinks.classList.toggle('active')
+})
+
